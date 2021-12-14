1866b68ef4e36027e4691513b3cbe2b0a64d1404
Soumil Sinha <sinha.5@iitj.ac.in>
2021-12-14 13:10:46 +0530

    changed

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+const popup = document.querySelector('.popup');
+const closebtn = document.querySelector('.ppclosebtn');
+const imgname = document.querySelector('.ppimgname');
+const arrowleft = document.querySelector('.left-arrow');
+const arrowright = document.querySelector('.right-arrow');
+const mainimage = document.querySelector('.ppmainimg');
+const images = [...document.querySelectorAll('.image')];
+
 function changeMode(){
     var element = document.body;
     element.classList.toggle("dark-mode")
@@ -7,4 +16,12 @@ function changeMode(){
     } else {
         document.getElementById("theme").src = "pics/DarkThemeIcon.png";
     }
+}
+
+function lhcPics(){
+    window.alert("hemloooo");
+    popup.classList.toggle('active');
+    images.forEach((item, i) => {
+        item.classList.toggle('blur');
+    })
 }
